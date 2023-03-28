126398a932be292ed6e00c541d9157a4e1b0d01b
Wilfred Erdo <wilfred05777@gmail.com>
2023-03-28 10:15:27 +0800

    BrowserRouter and routes configuration

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import main from '../assets/images/main.svg'
-import Wrapper from '../assets/wrappers/LandingPage'
-import { Logo } from '../components'
-import { Link } from 'react-router-dom'
-import { Navigate } from 'react-router-dom'
-import { useAppContext } from '../context/appContext'
+// import main from '../assets/images/main.svg'
+// import Wrapper from '../assets/wrappers/LandingPage'
+// import { Logo } from '../components'
+// import { Link } from 'react-router-dom'
+// import { Navigate } from 'react-router-dom'
+// import { useAppContext } from '../context/appContext'
 
 const Landing = () => {
   return (
