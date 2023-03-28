3d8bbc79ec2ea35c4fedf600254efa9fd2e55820
Wilfred Erdo <wilfred05777@gmail.com>
2023-03-28 10:06:08 +0800

    client install package

---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import main from '../assets/images/main.svg'
+import Wrapper from '../assets/wrappers/LandingPage'
+import { Logo } from '../components'
+import { Link } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
+import { useAppContext } from '../context/appContext'
+
+const Landing = () => {
+  return (
+    <div>
+      <h2>Landing</h2>
+    </div>
+  )
+}
+
+export default Landing
