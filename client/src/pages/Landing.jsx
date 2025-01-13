bbc9d2d2ce9b29e23c003237019b69eef16d56c9
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2025-01-14 05:28:50 +0800

    24. Wrapper - end

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
 // import main from '../assets/images/main.svg'
 // import Wrapper from '../assets/wrappers/LandingPage'
 // import { Logo } from '../components'
@@ -9,11 +11,23 @@ import { Link } from 'react-router-dom'
 
 const Landing = () => {
   return (
-    <div>
+    <Wrapper>
       <h2>Landing</h2>
       <Link to='/register'>Login / Register</Link>
-    </div>
+      <div className="content">Some Content</div>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  background: red;
+  h1{
+    color: white;
+  }
+  .content{
+    background: blue;
+    color: yellow;
+  }
+`;
 
 export default Landing
