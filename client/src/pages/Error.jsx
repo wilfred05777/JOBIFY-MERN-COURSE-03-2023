38a57aed42784c09f2c6ab14188e4d3d2f1722ce
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2025-01-14 11:11:39 +0800

    29. Error Page - initial commit: & bug on useRouterError() function

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,17 +1,38 @@
 import React from 'react'
 import img from '../assets/images/not-found.svg'
 import Wrapper from '../assets/wrappers/ErrorPage'
-import { Link } from 'react-router-dom'
+import { Link, useRouteError } from 'react-router-dom'
 
 const Error = () => {
+  // const error = useRouteError();
+  // // console.log(error);
+
+  // if(error.status === 404) {
+  //   return (
+  //     <Wrapper className='full-page'>
+  //       <div>
+  //         <img src={img} alt='not-found' />
+  //         <h3>Ohh! page not found</h3>
+  //         <p>We can't seem to find the page you're looking for</p>
+  //         <Link to={'/'}>back home</Link>
+  //       </div>
+  //     </Wrapper>
+  //   )
+  // }
+
   return (
     <Wrapper className='full-page'>
+
       <div>
         <img src={img} alt='not-found' />
         <h3>Ohh! page not found</h3>
         <p>We can't seem to find the page you're looking for</p>
         <Link to={'/'}>back home</Link>
       </div>
+
+      {/* <div>
+        Something went wrong
+      </div> */}
     </Wrapper>
   )
 }
