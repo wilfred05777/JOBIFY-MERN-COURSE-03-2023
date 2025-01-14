e4a2effe5c910a2207e7399e0931e89ee9b85a23
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2025-01-14 08:55:32 +0800

    Landing Page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import Wrapper from '../assets/wrappers/LandingPage'
+import main from '../assets/images/main.svg'
+import Logo from '../assets/images/logo.svg'
 
 // import main from '../assets/images/main.svg'
 // import Wrapper from '../assets/wrappers/LandingPage'
@@ -12,22 +15,35 @@ import styled from 'styled-components'
 const Landing = () => {
   return (
     <Wrapper>
-      <h2>Landing</h2>
+      <nav>
+        <img src={Logo} alt='logo' className='logo'/>
+      </nav>
+
+      <div className="container page">
+        <div className="info">
+          <h1>Job <span>Tracking</span>App</h1>
+          <p>I'm baby green juice cray trust fund post-ironic. Jean shorts irony adaptogen chartreuse synth try-hard air plant snackwave tumblr. Copper mug sriracha gluten-free poutine succulents. Migas blue bottle snackwave street art chicharrones echo park. </p>
+          <Link to='/register' className='btn'>Register</Link>
+          <Link to="/login" className='btn'>Login / Demo User</Link>
+        </div>
+        <img src={main} alt='main' className='main'/>
+      </div>
+      {/* <h2>Landing</h2>
       <Link to='/register'>Login / Register</Link>
-      <div className="content">Some Content</div>
+      <div className="content">Some Content</div> */}
     </Wrapper>
   )
 }
 
-const Wrapper = styled.div`
-  background: red;
-  h1{
-    color: white;
-  }
-  .content{
-    background: blue;
-    color: yellow;
-  }
-`;
+// const Wrapper = styled.div`
+//   background: red;
+//   h1{
+//     color: white;
+//   }
+//   .content{
+//     background: blue;
+//     color: yellow;
+//   }
+// `;
 
 export default Landing
