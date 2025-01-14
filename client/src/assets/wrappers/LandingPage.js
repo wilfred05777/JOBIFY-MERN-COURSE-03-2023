b07b6afa3222454eb09467f46f98e80f92e9744e
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2025-01-14 10:05:30 +0800

    26. Landing Page CSS styling

--- client/src/assets/wrappers/LandingPage.js
+++ client/src/assets/wrappers/LandingPage.js
@@ -1,7 +1,63 @@
 import styled from 'styled-components'
 
-const Wrapper = styled.main`
-  nav {
+const Wrapper = styled.section`
+
+  nav{
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    display: flex;
+    height: var(--nav-height);
+    align-items: center;
+    display:flex;
+  }
+
+  .page{
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+  }
+
+  h1{
+    font-weight: 700;
+    span{
+       color: var(--primary-500);
+    } 
+    margin-bottom: 1.5rem;
+  }
+
+  p{
+    line-height: 2;
+    color: var(--text-secondary-color);
+    margin-bottom: 1.5rem;
+    max-width: 35em;
+  }
+
+  .register-link {
+    margin-right: 1rem;
+  }
+
+  .main-img {
+    display: none;
+  }
+  
+  .btn{
+    padding: 0.75rem 1rem;
+  }
+
+  @media(min-width:992px){
+    .page{
+      grid-template-columns: 1fr 400px;
+      column-gap: 3rem;
+    }
+    .
+    .main-img{
+      display: block;
+    }
+  }
+
+  /* nav {
     width: var(--fluid-width);
     max-width: var(--max-width);
     margin: 0 auto;
@@ -35,6 +91,6 @@ const Wrapper = styled.main`
     .main-img {
       display: block;
     }
-  }
+  } */
 `
 export default Wrapper
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -21,14 +21,23 @@ const Landing = () => {
 
       <div className="container page">
         <div className="info">
-          <h1>Job <span>Tracking</span>App</h1>
+          <h1>
+            Job <span>Tracking</span>App
+          </h1>
           {/* resource for text generator https://hipsum.co/ */}
           <p>I'm baby green juice cray trust fund post-ironic. Jean shorts irony adaptogen chartreuse synth try-hard air plant snackwave tumblr. Copper mug sriracha gluten-free poutine succulents. Migas blue bottle snackwave street art chicharrones echo park. </p>
-          <Link to='/register' className='btn'>Register</Link>
-          <Link to="/login" className='btn'>Login / Demo User</Link>
+
+          <Link to='/register' className='btn register-link'>
+            Register
+          </Link>
+          <Link to="/login" className='btn register-link'>
+            Login / Demo User
+
+          </Link>
         </div>
-        <img src={main} alt='main' className='main'/>
+        <img src={main} alt='main' className='main-img img'/>
       </div>
+
       {/* <h2>Landing</h2>
       <Link to='/register'>Login / Register</Link>
       <div className="content">Some Content</div> */}
