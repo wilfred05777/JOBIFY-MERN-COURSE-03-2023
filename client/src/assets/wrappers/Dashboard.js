b676f40c9cf3217caccc7cdd4fbe46c9b317eecd
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2025-01-16 09:50:46 +0800

    36. Dashboard Structure - fixed bug on not /dashboard/admin - end

---
+++ client/src/assets/wrappers/Dashboard.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.main`
+    /* style here */
+
+`;
+
+export default Wrapper;
--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import {Wrapper} from '../assets/wrappers/Dashboard';
+// import Wrapper from '../assets/wrappers/Dashboard';
+import { BigSidebar, Navbar, SmallSidebar } from '../components'
 
 const DashboardLayout = () => {
   return (
-    <>
-      <Outlet/>
-    </>
+    <Wrapper>
+      <main className="dashboard">
+        <SmallSidebar />
+        <BigSidebar />
+        <div >
+          <Navbar />
+          <div className="dashboard-page">
+            <Outlet />
+          </div>
+        </div>
+      </main>
+    </Wrapper>
   )
 }
 
