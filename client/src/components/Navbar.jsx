8b82e6b369e69c0dfe94b80f785c2afb062f02f0
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2025-01-16 13:02:18 +0800

    40. Navbar Structure - end

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/Navbar";
-import { FaHome } from "react-icons/fa";
+import { FaAlignLeft, FaHome } from "react-icons/fa";
+import Logo from "./Logo";
+import { useDashboardContext } from "../pages/DashboardLayout";
 
 const Navbar = () => {
+   const { toggleSidebar } = useDashboardContext();
+
    return (
       <Wrapper>
-         <h2>navbar</h2>
-         <FaHome />
+         <div className='nav-center'>
+            <button
+               type='button'
+               className='toggle-btn'
+               onClick={toggleSidebar}
+            >
+               <FaAlignLeft />
+            </button>
+
+            <div>
+               <Logo />
+               <h4 className='logo-text'>dashboard</h4>
+            </div>
+            <div className='btn-container'>toggle/logout</div>
+         </div>
       </Wrapper>
    );
 };
--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,27 +1,42 @@
-import Landing from './Landing'
-import Register from './Register'
-import Admin from './Admin'
-import AddJob from './AddJob'
-import AllJobs from './AllJobs'
-import DashboardLayout from './DashboardLayout'
-import EditJob from './EditJob'
-import Error from './Error'
-import HomeLayout from './HomeLayout'
-import Login from './Login'
-import Stats from './Stats'
-import Profile from './Profile'
+export { default as DashboardLayout } from "./DashboardLayout";
+export { default as Landing } from "./Landing";
+export { default as HomeLayout } from "./HomeLayout";
+export { default as Register } from "./Register";
+export { default as Login } from "./Login";
+export { default as Error } from "./Error";
+export { default as Stats } from "./Stats";
+export { default as AllJobs } from "./AllJobs";
+export { default as AddJob } from "./AddJob";
+export { default as EditJob } from "./EditJob";
+export { default as Profile } from "./Profile";
+export { default as Admin } from "./Admin";
 
-export {
-    HomeLayout,
-    DashboardLayout,
-    Landing,
-    Register,
-    Admin,
-    AllJobs,
-    AddJob,
-    EditJob,
-    Error,
-    Login,
-    Profile,
-    Stats,
-}
+// Old version
+
+// import Landing from './Landing'
+// import Register from './Register'
+// import Admin from './Admin'
+// import AddJob from './AddJob'
+// import AllJobs from './AllJobs'
+// import DashboardLayout from './DashboardLayout'
+// import EditJob from './EditJob'
+// import Error from './Error'
+// import HomeLayout from './HomeLayout'
+// import Login from './Login'
+// import Stats from './Stats'
+// import Profile from './Profile'
+
+// export {
+//     HomeLayout,
+//     DashboardLayout,
+//     Landing,
+//     Register,
+//     Admin,
+//     AllJobs,
+//     AddJob,
+//     EditJob,
+//     Error,
+//     Login,
+//     Profile,
+//     Stats,
+// }
