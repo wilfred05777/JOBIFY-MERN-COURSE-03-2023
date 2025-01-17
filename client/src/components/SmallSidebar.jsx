4d736f2f1e34aecf6438f1851d62473c81b091e2
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2025-01-18 05:05:07 +0800

    44. Sidebar Functionality - end

--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -7,24 +7,42 @@ import links from "../utils/links.jsx";
 import { NavLink } from "react-router-dom";
 
 const SmallSidebar = () => {
-   const data = useDashboardContext();
-   console.log(data);
+   const { showSidebar, toggleSidebar } = useDashboardContext();
+   // showSidebar
+   // const data = useDashboardContext();
+   // console.log(data);
 
    return (
       <Wrapper>
-         <div className='sidebar-container show-sidebar'>
+         <div
+            className={
+               showSidebar
+                  ? "sidebar-container show-sidebar" // finished=if the icon is click it will pop-up navbar content, 1st on development only = if sidebar is within 992px width it will show
+                  : "sidebar-container" // otherwise it will hide the navbar once it is below 992px width
+            }
+         >
             <div className='content'>
-               <button type='button' className='close-btn'>
+               <button
+                  type='button'
+                  className='close-btn'
+                  onClick={toggleSidebar} // toggle close upon clicking the x icon
+               >
                   <FaTimes />
                </button>
                <header>
                   <Logo />
                </header>
                <div className='nav-links'>
+                  {/* below links from utils looping each content */}
                   {links.map((link) => {
-                     const { text, path, icon } = link;
+                     const { text, path, icon } = link; // assign each array content to link
                      return (
-                        <NavLink to={path} key={text} className='nav-link'>
+                        <NavLink
+                           to={path}
+                           key={text}
+                           className='nav-link'
+                           onClick={toggleSidebar} // navigate to each assign path from the links
+                        >
                            <span className='icon'>{icon}</span>
                            {text}
                         </NavLink>
