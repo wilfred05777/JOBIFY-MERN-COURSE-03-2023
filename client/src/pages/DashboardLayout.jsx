8ca06e09d459e93588c7a0386d8e9892aeb08157
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2025-03-28 15:48:45 +0800

    49. Logout Container - console  not working

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -7,48 +7,52 @@ import { BigSidebar, Navbar, SmallSidebar } from "../components";
 const DashbardContext = createContext();
 
 const DashboardLayout = () => {
-   // temp
-   const user = { name: "john" };
+  // temp
+  const user = { name: "john" };
 
-   const [showSidebar, setShowSidebar] = useState(false);
-   const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
 
-   const toggleDarkTheme = () => {
-      console.log("Toggle Dark theme");
-   };
+  const toggleDarkTheme = () => {
+    console.log("Toggle Dark theme");
+  };
 
-   const toggleSidebar = () => {
-      setShowSidebar(!showSidebar);
-   };
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
 
-   const logoutUser = async () => {
-      console.log("logout user");
-   };
+  const logoutUser = async () => {
+    console.log("logout user");
+    //  navigate("/");
+    //  await customFetch.get("/auth/logout");
+    //  queryClient.invalidateQueries();
+    //  toast.success("Logging out...");
+  };
 
-   return (
-      <DashbardContext.Provider
-         value={{
-            user,
-            showSidebar,
-            isDarkTheme,
-            toggleSidebar,
-            toggleDarkTheme,
-         }}
-      >
-         <Wrapper>
-            <main className='dashboard'>
-               <SmallSidebar />
-               <BigSidebar />
-               <div>
-                  <Navbar />
-                  <div className='dashboard-page'>
-                     <Outlet />
-                  </div>
-               </div>
-            </main>
-         </Wrapper>
-      </DashbardContext.Provider>
-   );
+  return (
+    <DashbardContext.Provider
+      value={{
+        user,
+        showSidebar,
+        isDarkTheme,
+        toggleSidebar,
+        toggleDarkTheme
+      }}
+    >
+      <Wrapper>
+        <main className="dashboard">
+          <SmallSidebar />
+          <BigSidebar />
+          <div>
+            <Navbar />
+            <div className="dashboard-page">
+              <Outlet />
+            </div>
+          </div>
+        </main>
+      </Wrapper>
+    </DashbardContext.Provider>
+  );
 };
 
 export const useDashboardContext = () => useContext(DashbardContext);
